21817207b6fd8e5d7d95ab8f85e42e37c7e23907
Janitha Meedeniya <m3janitha@gmail.com>
2025-02-04 18:45:50 +0000

    github workflows benchmark

--- .github/run_sim.sh
+++ .github/run_sim.sh
@@ -1,6 +1,6 @@
 $GITHUB_WORKSPACE/build/examples/trading_system/application/sim_server_b "127.0.0.1" "8086" "" "0" "tcp" "server" "serverB" "10" > $GITHUB_WORKSPACE/sim_server_b.log 2>&1 &
 sleep 1
-$GITHUB_WORKSPACE/build/examples/trading_system/application/app_trading_system "" > $GITHUB_WORKSPACE/app_trading_system.log 2>&1 &
+$GITHUB_WORKSPACE/build/examples/trading_system/application/app_trading_system "$GITHUB_WORKSPACE/examples/trading_system/config/config.xml" > $GITHUB_WORKSPACE/app_trading_system.log 2>&1 &
 sleep 1
 $GITHUB_WORKSPACE/build/examples/trading_system/application/sim_client_a "" "0" "127.0.0.1" "8081" "tcp" "client" "clientA" "100" > $GITHUB_WORKSPACE/sim_client_a.log 2>&1 &
 sleep 15
